46eaadf2a3a71225970ff0c6df0e2baccea04f75
Tommie Nygren <tommie.nygren@beamonpeople.se>
2015-12-21 12:43:15 +0000

    Sparar personer i db, många många, bra

--- main.go
+++ main.go
@@ -2,14 +2,42 @@ package main
 
 import (
     "fmt"
-    "html"
     "log"
     "net/http"
+    "gopkg.in/mgo.v2"
+    "gopkg.in/mgo.v2/bson"
 )
 
+type Person struct {
+        Name string
+        Info string
+}
+
 func main() {
+
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+      session, err := mgo.Dial("localhost")
+      if err != nil {
+             panic(err)
+      }
+      defer session.Close()
+
+      // Optional. Switch the session to a monotonic behavior.
+      session.SetMode(mgo.Monotonic, true)
+
+      c := session.DB("autocv").C("people")
+      err = c.Insert(&Person{"Doopy Dorp", "Nice but dumb"},
+               &Person{"Clara Cloora", "Retard"})
+      if err != nil {
+             log.Fatal(err)
+      }
+
+      result := Person{}
+      err = c.Find(bson.M{"name": "Doopy Dorp"}).One(&result)
+      if err != nil {
+             log.Fatal(err)
+      }
+      fmt.Fprintf(w, "Name: %q", result.Name)
     })
 
     log.Fatal(http.ListenAndServe(":8080", nil))
