899527ce641ad726cd9f5ca9a3d8f1114bfd7d34
Tommie Nygren <tommie.nygren@beamonpeople.se>
2015-12-22 12:48:03 +0000

    Första utkast

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,10 +1,23 @@
 var autocv = angular.module('autocv', ['ui.router']);
 
+//Route Config
 autocv.config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise("/home");
 
   $stateProvider
     .state('home', {
-      
+      url: "/home",
+      templateUrl: "templates/peoplelist.html",
+      controller: "PeopleCtrl"
     })
+});
+
+//People List Controller
+autocv.controller('PeopleCtrl', function($scope, $http) {
+  $scope.people = [];
+  $http.get('/api/people/').success(function(data) {
+    $scope.people = data;
+  }).error(function(msg, code) {
+    console.log(msg);
+  })
 });
