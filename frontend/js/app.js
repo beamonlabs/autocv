baf5bc2a8b5097fcfe3408e9f18c385257711bcc
Tommie Nygren <tommie.nygren@beamonpeople.se>
2015-12-22 13:55:57 +0000

    Skapat route för editsidan, gör så att personen laddas när man går till den sidan

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -10,10 +10,15 @@ autocv.config(function($stateProvider, $urlRouterProvider){
       templateUrl: "templates/peoplelist.html",
       controller: "PeopleCtrl"
     })
+    .state('edit', {
+      url:"/edit/:id",
+      templateUrl: "templates/editperson.html",
+      controller: "EditCtrl"
+    })
 });
 
 //People List Controller
-autocv.controller('PeopleCtrl', function($scope, $http) {
+autocv.controller('PeopleCtrl', function($scope, $http, $state) {
   $scope.people = [];
   $http.get('/api/people/').success(function(data) {
     $scope.people = data;
@@ -27,4 +32,17 @@ autocv.controller('PeopleCtrl', function($scope, $http) {
       console.log(msg);
     });
   }
+
+  $scope.editPerson = function(id) {
+    $state.go("edit", {"id": id });
+  }
+});
+
+autocv.controller('EditCtrl', function($scope, $http, $state, $stateParams) {
+  $scope.person = {};
+  $http.get('api/people/' + $stateParams.id).success(function(data) {
+    $scope.person = data;
+  }).error(function(msg, code) {
+    console.log(msg);
+  })
 });
