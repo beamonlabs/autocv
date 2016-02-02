8f9db8d82da7c2a2ed72a54e1ef82acd0cf1ec39
andreas gustafsson <andreas.gustafsson@beamonpeople.se>
2016-02-02 21:25:07 +0000

    startstrict for mongo connstring

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-IP=$(getent hosts mongo | awk '{print $1}')
-PORT=$MONGO_PORT
-CONNSTRING="mongodb://$IP:$PORT"
-ehco $CONNSTRING
+#IP=$(getent hosts mongo | awk '{print $1}')
+#PORT=$MONGO_PORT
+CONNSTRING="${MONGO_PORT/tcp/mongodb}"
+echo $CONNSTRING
 ./main $CONNSTRING
